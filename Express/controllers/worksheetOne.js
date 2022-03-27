a49825b18d7d0604cd01f9c9b91b86de58273c1e
Gerry Pratama <gerryajie@gmail.com>
2022-03-28 00:56:36 +0700

    commitan

--- Express/controllers/worksheetOne.js
+++ Express/controllers/worksheetOne.js
@@ -7,40 +7,38 @@ const upload = async (req, res) => {
       return res.status(400).send("Please upload an excel file!");
     }
     // req.files.file.tempFilePath;
-    readXlsxFile(req.files.file.tempFilePath, { sheet: 1, sheet: 2 }).then(
-      (rows) => {
-        // skip header
-        rows.shift();
-        let invoiceArr = [];
-        rows.forEach((row) => {
-          let invoiceData = {
-            noInvoice: row[0],
-            date: row[1],
-            customerName: row[2],
-            salesPersonName: row[3],
-            paymentType: row[4],
-            notes: row[5],
-          };
+    readXlsxFile(req.files.file.tempFilePath, { sheet: 1 }).then((rows) => {
+      // skip header
+      rows.shift();
+      let invoiceArr = [];
+      rows.forEach((row) => {
+        let invoiceData = {
+          noInvoice: row[0],
+          date: row[1],
+          customerName: row[2],
+          salesPersonName: row[3],
+          paymentType: row[4],
+          notes: row[5],
+        };
 
-          invoiceArr.push(invoiceData);
-        });
+        invoiceArr.push(invoiceData);
+      });
 
-        invoice
-          .bulkCreate(invoiceArr)
-          .then(() => {
-            res.status(200).send({
-              message: "Uploaded the file successfully: " + req.files.file.name,
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            return res.status(500).send({
-              message: "Fail to import data into database!",
-              error: error.message,
-            });
+      invoice
+        .bulkCreate(invoiceArr)
+        .then(() => {
+          res.status(200).send({
+            message: "Uploaded the file successfully: " + req.files.file.name,
           });
-      }
-    );
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.status(500).send({
+            message: "Fail to import data into database!",
+            error: error.message,
+          });
+        });
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
